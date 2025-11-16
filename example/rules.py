dc2eab03954018a6cfdcc4d1deb34a31d602c855
Matt Craighead <mjcraighead@gmail.com>
2025-11-16 12:05:11 -0600

    Add some comments to example rules.py and make it a bit easier to follow

--- example/rules.py
+++ example/rules.py
@@ -1,15 +1,19 @@
 def rules(ctx):
+    # Source files
     c_files = ['main.c']
 
+    # Compile each .c file into a .o with an accompanying depfile
     o_files = []
-    for file in c_files:
-        o_file = '_out/%s' % file.replace('.c', '.o')
+    for src in c_files:
+        o_file = f"_out/{src.replace('.c', '.o')}"
         depfile = o_file.replace('.o', '.d')
-        ctx.rule(o_file, file, cmd=['gcc', '-o', o_file, '-c', file, '-MD'], depfile=depfile)
-        o_files += [o_file]
+        ctx.rule(o_file, src, cmd=['gcc', '-o', o_file, '-c', src, '-MD'], depfile=depfile)
+        o_files.append(o_file)
 
+    # Link all objects into the final executable
     exe_file = '_out/hello'
-    ctx.rule(exe_file, o_files, cmd=['gcc', '-o', exe_file] + o_files)
+    ctx.rule(exe_file, o_files, cmd=['gcc', '-o', exe_file, *o_files])
 
+    # Phony aggregate targets
     ctx.rule(':build', exe_file)
     ctx.rule(':all', ':build')
